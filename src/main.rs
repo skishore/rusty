182b6e24343794342773e23dd9b5b798dad7a085
Shaunak Kishore <kshaunak@gmail.com>
2023-09-18 23:29:56 -0400

    Basic game loop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,78 @@
 mod base;
 
 use base::{Cell, Token};
+use std::io::{self, StdoutLock, Write};
+
+use game_loop::game_loop;
+use rand::random;
+
+#[derive(Eq, Hash, PartialEq)]
+struct Point(i32, i32);
+
+struct Entity { pos: Point, vel: Point }
+
+struct Game { entities: Vec<Entity>, size: Point }
+
+impl Game {
+    fn update(&mut self) {
+        for entity in self.entities.iter_mut() {
+            entity.pos.0 = entity.pos.0 + entity.vel.0;
+            entity.pos.1 = entity.pos.1 + entity.vel.1;
+            if entity.pos.0 < 0 {
+                entity.pos.0 = -entity.pos.0;
+                entity.vel.0 = -entity.vel.0;
+            } else if entity.pos.0 >= self.size.0 {
+                entity.pos.0 = 2 * self.size.0 - entity.pos.0 - 1;
+                entity.vel.0 = -entity.vel.0;
+            }
+            if entity.pos.1 < 1 {
+                entity.pos.1 = -entity.pos.1;
+                entity.vel.1 = -entity.vel.1;
+            } else if entity.pos.1 >= self.size.1 {
+                entity.pos.1 = 2 * self.size.1 - entity.pos.1 - 1;
+                entity.vel.1 = -entity.vel.1;
+            }
+        }
+    }
+
+    fn render(&self) {
+        let mut lock = io::stdout().lock();
+        self.clear_screen(&mut lock);
+        self.move_cursor(Point(0, 0), &mut lock);
+        for y in 0..self.size.1 {
+            for x in 0..self.size.0 {
+                let point = Point(x, y);
+                let blocked = self.entities.iter().any(|x| x.pos == point);
+                if blocked { write!(lock, "XX") } else { write!(lock, "  ") }.ok();
+            }
+            write!(lock, "\n").ok();
+        }
+    }
+
+    fn clear_screen(&self, lock: &mut StdoutLock) {
+        write!(lock, "\x1b[2J").ok();
+    }
+
+    fn move_cursor(&self, point: Point, lock: &mut StdoutLock) {
+        write!(lock, "\x1b[{};{}H", point.1 + 1, point.0 + 1).ok();
+    }
+}
 
 fn main() {
-    let mut t = unsafe { Token::new() };
-    let x: Cell<i32> = Cell::new(17);
-    let y: Cell<i32> = Cell::new(34);
-    *x.get_mut(&mut t) = 51;
-    *y.get_mut(&mut t) = 64;
-    let z = {
-        let a = x.get(&t);
-        let b = y.get(&t);
-        a + b
-    };
-    println!("Final value: {:?}", z);
+    let mut game = Game { entities: vec![], size: Point(40, 40) };
+
+    for _ in 0..3 {
+        let x = random::<i32>() % game.size.0;
+        let y = random::<i32>() % game.size.1;
+        let vx = if random::<bool>() { 1 } else { -1 };
+        let vy = if random::<bool>() { 1 } else { -1 };
+        game.entities.push(Entity { pos: Point(x, y), vel: Point(vx, vy) });
+    }
+
+    game_loop(game, 6, 0.1, |g| {
+        g.game.update()
+    }, |g| {
+        g.game.render();
+        std::thread::sleep(std::time::Duration::from_micros(800));
+    });
 }
