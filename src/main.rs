85a8b8895d9ea54276ff206b001cb54a4115849d
Shaunak Kishore <kshaunak@gmail.com>
2023-09-23 12:31:09 -0400

    Use termion for screen interactions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod base;
 
-use base::{Cell, Token};
-use std::io::{self, StdoutLock, Write};
+use std::io::{self, Write};
 
 use game_loop::game_loop;
 use rand::random;
+use termion::cursor::{Goto, Hide, Show};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::screen::{ToAlternateScreen, ToMainScreen};
+
+type Screen = termion::raw::RawTerminal<io::Stdout>;
 
 #[derive(Eq, Hash, PartialEq)]
 struct Point(i32, i32);
@@ -35,30 +41,40 @@ impl Game {
         }
     }
 
-    fn render(&self) {
-        let mut lock = io::stdout().lock();
-        self.clear_screen(&mut lock);
-        self.move_cursor(Point(0, 0), &mut lock);
+    fn render(&self, screen: &mut Screen) {
         for y in 0..self.size.1 {
+            self.move_cursor(Point(0, y), screen);
             for x in 0..self.size.0 {
                 let point = Point(x, y);
                 let blocked = self.entities.iter().any(|x| x.pos == point);
-                if blocked { write!(lock, "XX") } else { write!(lock, "  ") }.ok();
+                if blocked { write!(screen, "XX") } else { write!(screen, "  ") }.unwrap();
             }
-            write!(lock, "\n").ok();
         }
+        screen.flush().ok();
     }
 
-    fn clear_screen(&self, lock: &mut StdoutLock) {
-        write!(lock, "\x1b[2J").ok();
-    }
-
-    fn move_cursor(&self, point: Point, lock: &mut StdoutLock) {
-        write!(lock, "\x1b[{};{}H", point.1 + 1, point.0 + 1).ok();
+    fn move_cursor(&self, point: Point, out: &mut Screen) {
+        let (x, y) = (point.0, point.1);
+        write!(out, "{}", Goto((x + 1) as u16, (y + 1) as u16)).unwrap();
     }
 }
 
+fn enter_alt_screen(screen: &mut Screen) {
+    write!(screen, "{}{}{}", ToAlternateScreen, Hide, termion::clear::All).unwrap();
+    write!(screen, "{}", termion::clear::All).unwrap();
+    screen.flush().unwrap();
+}
+
+fn exit_alt_screen(screen: &mut Screen) {
+    write!(screen, "{}{}", ToMainScreen, Show).unwrap();
+    screen.flush().unwrap();
+}
+
 fn main() {
+    let mut inputs = termion::async_stdin().keys();
+    let mut screen = io::stdout().into_raw_mode().unwrap();
+    enter_alt_screen(&mut screen);
+
     let mut game = Game { entities: vec![], size: Point(40, 40) };
 
     for _ in 0..3 {
@@ -70,9 +86,16 @@ fn main() {
     }
 
     game_loop(game, 6, 0.1, |g| {
+        if let Some(Ok(key)) = inputs.next() {
+            if key == Key::Char('q') || key == Key::Ctrl('c') {
+                g.exit();
+            }
+        }
         g.game.update()
     }, |g| {
-        g.game.render();
+        g.game.render(&mut screen);
         std::thread::sleep(std::time::Duration::from_micros(800));
     });
+
+    exit_alt_screen(&mut screen);
 }
